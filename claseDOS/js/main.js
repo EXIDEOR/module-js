6e171badf1a8ec6f94ea1220041c136c54e66595
Fego Aguilar <89560451+EXIDEOR@users.noreply.github.com>
2021-10-03 17:35:49 -0500

    Add files via upload

---
+++ claseDOS/js/main.js
@@ -0,0 +1,115 @@
+
+//DECLARAR VARIABLE SALUDO
+    //let SALU2
+//ASIGNAR VALOR CADENA A VARIABLE SALUDO
+//IMPRIMIR EN CONSOLA LA VARIABLE SALUDO
+
+// let SALU2 = "HOLAKODERS"
+
+// console.log(SALU2)
+
+//EJERCICIO 1
+
+// let x = 6
+// let y = 3
+
+// console.log(`la suma es ${x+y}`)
+// console.log(`la resta es ${y-x}`)
+// console.log(`la multiplicacion es ${x*y}`)
+// console.log(`la resta es ${y/x}`)
+
+//EJERCICIO 2
+
+// let objetoUno="apple", objetoDos="pen";
+// console.log(objetoUno+objetoDos);
+
+// let numeroDia;
+
+// switch (numeroDia) {
+//     case value:1
+//         console.log("lunes")
+//         break;
+    
+//     case value:2
+//         console.log("martes")
+//         break;
+    
+//     case value:3
+//         console.log("miercoles")
+//         break;
+
+//     case value:4
+//         console.log("jueves")
+//         break;
+
+//     case value:5
+//         console.log("viernes")
+//         break;
+        
+//     case value:6
+//         console.log("sabado")
+//         break;
+// }
+
+// if (x > y) {
+//     console.log("x es mayor que y")
+// }
+// else {
+//     console.log("x es menor que y")
+// }
+
+// if (y < x) {
+//     console.log("y es menor que x")
+// }
+// else {
+//     console.log("x es mayor que y")
+// }
+
+// const currentDay =0;
+
+// switch (currentDay) {
+//     case 0:
+//         const dia = 'domingo'
+//         break
+//     case 1:
+//         const dia = 'lunes'
+//         break
+// }
+
+// const age = 6;
+// const resultado = age > 18 ? 'es adulto' . 'No es adulto' ;
+// console.log (resultado)
+
+
+
+//ejercicio 1
+
+num = prompt ('ingresar numero');
+document.write(num);
+
+if (num%2==0){
+    console.log ("es par")
+}
+
+else{
+    console.log ("Es impar")
+}
+v
+//ejercicio 2
+let num1 = prompt('digite un numero')
+let num2 = prompt('digite un segundo numero')
+
+if (num1>num2) {
+    console.log('num2 es menor')
+}
+else{
+    console.log('num1 es mayor')
+}
+
+//ejercicio 3
+function pesolunar() {
+    let nombre, pesoTierra, pesoLunar, gravedadTierra - 9.8, gravedadLuna - 1.622;
+
+    nombre - String(prompt('ingresar nombre'));
+    pesoTierra = NU
+}
